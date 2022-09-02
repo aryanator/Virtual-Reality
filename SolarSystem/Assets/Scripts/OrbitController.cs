7f7a814bad71408a34af61b625426c2d4063a25c
aaaryan17 <84935470+aaaryan17@users.noreply.github.com>
2022-09-02 16:26:13 +0530

    jhb

---
+++ SolarSystem/Assets/Scripts/OrbitController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OrbitController : MonoBehaviour
+{
+    public Transform orbitPivot;
+    public float orbitSpeed;
+    public float rotationSpeed;
+    public float planetRadius;
+    public float distFromStar;
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Creates a random position along the orbit path
+        Vector2 randomPosition = Random.insideUnitCircle;
+        transform.position = new Vector3(randomPosition.x, 0f,
+        randomPosition.y) * distFromStar;
+        // Sets the size of the GameObject to the Planet radius value
+        transform.localScale = Vector3.one * planetRadius;
+    
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.transform.RotateAround(orbitPivot.position,
+         Vector3.up, orbitSpeed * Time.deltaTime);
+        this.transform.Rotate(Vector3.up, rotationSpeed *
+        Time.deltaTime);
+
+
+    }
+}
